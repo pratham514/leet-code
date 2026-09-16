3c84ef28bd67ec9be8992de9743d22e9f4acace2
pratham chawla <prathamchawla1308@gmail.com>
2026-09-16 14:02:33 +0530

    Time: 3 ms (68.9%), Space: 26.8 MB (14.07%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool helper(int i,int j,string s){
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int i=0;
+        int j=s.length()-1;
+        while (i<j){
+            if(s[i]!=s[j]){
+                return helper(i+1,j,s) || helper(i,j-1,s);
+                
+
+                
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
